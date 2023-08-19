a7e4dd1359fd06b666825775b63c5b97f0a570b5
Michael Loh <michael.loh@virginmediao2.co.uk>
2023-08-19 13:48:02 +0100

    fastapi unit test

--- src/main.py
+++ src/main.py
@@ -1,7 +1,17 @@
+import json
 import logging.config
+import time
 from os import path
 
-from fastapi import FastAPI
+from fastapi import FastAPI, status, Request
+from fastapi.encoders import jsonable_encoder
+from fastapi.exceptions import RequestValidationError
+from fastapi.responses import JSONResponse
+from starlette.responses import Response
+
+from cloud.datastore import Datastore
+from cloud.pubsub import decode_pubsub_data
+from model.pubsub import PubSubRequest
 
 logging.config.fileConfig(
     path.join(path.dirname(path.abspath(__file__)), 'config/logging.ini'),
@@ -17,7 +27,55 @@ app = FastAPI(title="mloh-sandbox")
 
 # Health check endpoint to determine if the service is up and running
 @app.get("/health")
-async def health_check():
+def health_check():
     """Health check response"""
     logger.info("health check endpoint")
     return {"health": "ok"}
+
+
+@app.post("/v1/transfer_funds", response_model=dict)
+def update_datastore(request: PubSubRequest):
+    decoded_data = decode_pubsub_data(request.message.data)
+    Datastore().transfer_funds(from_key=decoded_data['from'], to_key=decoded_data['to'],
+                               amount=decoded_data['amount'])
+    return JSONResponse(status_code=status.HTTP_200_OK,
+                        content=jsonable_encoder({"message_id": request.message.messageId}))
+
+
+@app.exception_handler(RequestValidationError)
+async def validation_exception_handler(request: Request, exc: RequestValidationError):
+    return JSONResponse(
+        status_code=status.HTTP_422_UNPROCESSABLE_ENTITY,
+        content=jsonable_encoder({"detail": exc.errors(), "body": exc.body}),
+    )
+
+
+@app.middleware("http")
+async def api_middleware(request: Request, call_next):
+    # Count request time performance
+    start_time = time.perf_counter()
+    response = await call_next(request)
+    body = b""
+    async for data in response.body_iterator:
+        body += data
+    if len(body) != 0:
+        message_body = json.loads(body)
+        try:
+            message_id = message_body["message_id"]
+            status = message_body["status"]
+            detail = message_body["detail"]
+        except KeyError as exc:
+            message_id = None
+            status = "Failed"
+            detail = str(exc)
+    process_time_ms = (time.perf_counter() - start_time) * 1000
+    logger.info(
+        f"Processing Stats (Request:{request.scope['path']}---Time:{process_time_ms:.1f} milliseconds---",
+        {"httpStatusCode": response.status_code},
+    )
+    logger.info(f"Message ID: {message_id}; status: {status}, response_code: {response.status_code}, detail: {detail}")
+
+    # Send back a new Response object. Required because the body was extracted for logging
+    return Response(
+        content=body, status_code=response.status_code, headers=response.headers
+    )
--- tests/unit_tests/test_main.py
+++ tests/unit_tests/test_main.py
@@ -1,3 +1,10 @@
+import base64
+import json
+from unittest.mock import patch
+
+from cloud.datastore import Datastore
+
+
 def test_health(api_client):
     """
     WHEN reading from the ping endpoint
@@ -5,4 +12,19 @@ def test_health(api_client):
     """
     response = api_client.get("/health")
     assert response.status_code == 200
-    assert response.json()["status"] == {"health": "ok"}
+    assert response.json() == {"health": "ok"}
+
+
+@patch.object(target=Datastore, attribute='transfer_funds', return_value=None)
+def test_transfer_funds_success(mock_transfer_funds, api_client):
+    transfer_funds_request = {"from": "q1w2e3", "to": "r4t5y6", "amount": 50}
+    request_body = {
+        "message": {
+            "data": base64.b64encode(json.dumps(transfer_funds_request).encode('utf-8')).decode('utf-8'),
+            "messageId": "123",
+            "publishTime": "2023-01-01T00:00:00.000000Z"
+        }
+    }
+    response = api_client.post("/v1/transfer_funds", json=request_body)
+    assert response.status_code == 200
+    assert response.json() == {'message_id': '123'}
